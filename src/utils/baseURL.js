d1d656dc1c4e4b246638706ec2e754f1cc87dff6
kris <k.100.huynh@gmail.com>
2025-06-13 22:41:24 +0700

    update url new

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,6 @@
 const getBaseUrl = () => {
-   return 'https://book-app-backend-cyan.vercel.app';
+   return 'https://book-app-backend-blue.vercel.app';
+   // return 'http://localhost:5000';
 };
 
 export default getBaseUrl;
